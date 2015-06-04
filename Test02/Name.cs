672bdc19e9046e1c2582fb7ffa0c6a5f4ad9ebfc
Alex Pearson <pearsonn.alex@gmail.com>
2015-06-04 19:01:27 +0000

    Add project and test files

---
+++ Test02/Name.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Test02
+{
+    /// <summary>
+    /// Represents a first and last name
+    /// </summary>
+    public class Name : IEquatable<Name>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Name(string first, string last)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// First name
+        /// </summary>
+        public string First { get; protected set; }
+
+        /// <summary>
+        /// Last name
+        /// </summary>
+        public string Last { get; protected set; }
+
+        /// <summary>
+        /// Returns the full name
+        /// </summary>
+        public string Full
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Create a new name with a different first name
+        /// </summary>
+        public Name ChangeFirst(string first)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Create a new name with a different last name
+        /// </summary>
+        public Name ChangeLast(string last)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks for equality
+        /// </summary>
+        public bool Equals(Name other)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region "Pre-built code"
+        private static readonly Name none = new Name { First = string.Empty, Last = string.Empty };
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        private Name()
+        {
+
+        }
+
+        /// <summary>
+        /// Represents an empty name
+        /// </summary>
+        /// <remarks>You should not need to touch this.  It represents an empty name</remarks>
+        public static Name None
+        {
+            get
+            {
+                return none;
+            }
+        }
+
+        /// <summary>
+        /// Gets the hash code of the name
+        /// </summary>
+        /// <remarks>You should not need to touch this.  Feel free to look up what it's for</remarks>
+        public override int GetHashCode()
+        {
+            return First.GetHashCode() ^ Last.GetHashCode();
+        }
+
+        /// <summary>
+        /// Checks for equality
+        /// </summary>
+        /// <remarks>You should not need to touch this.  Feel free to look up what it's for</remarks>
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Name);
+        }
+        #endregion
+    }
+}
---
+++ Test02/NameTest.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Xunit;
+
+namespace Test02
+{
+    public class NameTest
+    {
+        [Theory, InlineData(null), InlineData("")]
+        public void Constructor_ThrowsArgumentException_WhenFirstNameIsEmpty(string testPart)
+        {
+            Assert.Throws<ArgumentException>(() => new Name(testPart, "Doe"));
+        }
+
+        [Fact]
+        public void Constructor_SetsFirstAndLastNames_WhenNamesAreValid()
+        {
+            Name name = new Name("John", "Doe");
+            Assert.Equal(name.First, "John");
+            Assert.Equal(name.Last, "Doe");
+        }
+
+        [Fact]
+        public void FullName_ReturnsFirstAndLastNameWithSpace_WhenFirstAndLastNameAreSet()
+        {
+            Name customer = new Name("John", "Doe");
+
+            Assert.Equal(customer.Full, "John Doe");
+        }
+
+        [Fact]
+        public void FullName_ReturnsFirstNameWithNoSpace_WhenLastNameIsEmpty()
+        {
+            Name customer = Name.None.ChangeFirst("John");
+
+            Assert.Equal(customer.Full, "John");
+        }
+
+        [Fact]
+        public void FullName_ReturnsLastNameWithNoSpace_WhenFirstNameIsEmpty()
+        {
+            Name customer = Name.None.ChangeLast("Doe");
+
+            Assert.Equal(customer.Full, "Doe");
+        }
+
+        [Fact]
+        public void FullName_ReturnsEmptyString_WhenFirstAndLastNamesAreEmpty()
+        {
+            Assert.Empty(Name.None.Full);
+        }
+
+        [Fact]
+        public void Equals_ReturnsTrue_WhenFirstAndLastNameMatch()
+        {
+            Name name = new Name("John", "Doe");
+            bool result = name.Equals(new Name("John", "Doe"));
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Equals_ReturnsFalse_WhenFirstNamesMatchButLastNamesDont()
+        {
+            Name name = new Name("John", "Doe");
+            bool result = name.Equals(new Name("John", "Foo"));
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Equals_ReturnsFalse_WhenLastNamesMatchButFirstNamesDont()
+        {
+            Name name = new Name("John", "Doe");
+            bool result = name.Equals(new Name("Foo", "Doe"));
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ChangeFirstName_ReturnsNewName_WithUpdatedFirstName()
+        {
+            Name name = new Name("John", "Doe");
+            Name changed = name.ChangeFirst("Foo");
+
+            Assert.Equal(changed.First, "Foo");
+            Assert.Equal(changed.Last, "Doe");
+        }
+
+        [Theory, InlineData(null), InlineData("")]
+        public void ChangeFirstName_ThrowsArgumentException_WhenUpdatedFirstNameIsEmpty(string namePart)
+        {
+            Name name = new Name("John", "Doe");
+            Assert.Throws<ArgumentException>(() => name.ChangeFirst(namePart));
+        }
+
+        [Fact]
+        public void ChangeLastName_ReturnsNewName_WithUpdatedLastName()
+        {
+            Name name = new Name("John", "Doe");
+            Name changed = name.ChangeLast("Foo");
+
+            Assert.Equal(changed.First, "John");
+            Assert.Equal(changed.Last, "Foo");
+        }
+
+        [Theory, InlineData(null), InlineData("")]
+        public void ChangeLastName_ThrowsArgumentException_WhenUpdatedLastNameIsEmpty(string namePart)
+        {
+            Name name = new Name("John", "Doe");
+            Assert.Throws<ArgumentException>(() => name.ChangeLast(namePart));
+        }
+    }
+}
